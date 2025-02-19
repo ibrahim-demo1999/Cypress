b671808ab156542bca98a068b352fcbedff97b88
ibrahim <himodego@gmail.com>
2025-02-19 22:19:43 +0300

    ibrahim

--- Project 1/Cypress/cypress/e2e/spec.cy.js
+++ Project 1/Cypress/cypress/e2e/spec.cy.js
@@ -1,1 +1,52 @@
-//write ur tests here
+describe("tests", () => {
+  it("Cannot log in with username only", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="username-input"]').type("bash");
+    cy.get('[data-cy="password-input"]').contains(/[a-zA-z]/);
+    cy.get('[data-cy="submit-button"]').click();
+  });
+  it("Cannot log in with password only", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="password-input"]').type("password123");
+    cy.get('[data-cy="username-input"]').contains(/[a-zA-z]/);
+    cy.get('[data-cy="submit-button"]').click();
+  });
+  it(" Username must start and end with", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="username-input"]').type("bash");
+    cy.get('[data-cy="username-input"]').contains("#bash#");
+    cy.get('[data-cy="password-input"]').type("password123");
+    cy.get('[data-cy="submit-button"]').click();
+  });
+  it("option 2 test", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#dropdown").select("option2");
+  });
+  it("option 1 test", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#dropdown").select("option2").select("option1");
+  });
+  it("checked ", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="agree-checkbox"]').check();
+    cy.get("#checkbox-message").contains("You agreed to the terms!");
+    cy.get('[data-cy="agree-checkbox"]').uncheck();
+    cy.get("#checkbox-message").contains("You disagreed with the terms.");
+  });
+  it("2 click ", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="double-click-area"]').dblclick();
+  });
+  it("r click", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="right-click-area"]').rightclick();
+  });
+  it("drog drop", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="drag-item"]').drag("#drop-zone");
+  });
+  it("scrol", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#scroll-container").scrollTo("bottom");
+  });
+});
--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    experimentalStudio: true,
+    
   },
-};
+});
